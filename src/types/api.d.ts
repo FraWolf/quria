02274ff01e5ee084fdadef70506fe12f6158006d
Francesco <frawolf@outlook.it>
2021-12-17 19:19:13 +0100

    feat: added new types

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,6 +1,6 @@
 export interface APIResponse<T> {
   Response: T;
-  ErroreCode: number;
+  ErroreCode: number; // wip
   ThrottleSeconds: number;
   ErrorStatus: string;
   Message: string;
@@ -10,4 +10,16 @@ export interface APIResponse<T> {
 
 export interface MessageData {
   [key: string]: string;
+}
+
+export const enum MembershipTypes {
+  None = 0,
+  TigerXbox = 1,
+  TigerPsn = 2,
+  TigerSteam = 3,
+  TigerBlizzard = 4,
+  TigerStadia = 5,
+  TigerDemon = 10,
+  BungieNext = 254,
+  All = -1,
 }
--- src/types/destiny.d.ts
+++ src/types/destiny.d.ts
@@ -1,3 +1,5 @@
+import { MembershipTypes } from "./api";
+
 export interface DestinyEntityDefinition {
   displayProperties: {
     description: string;
@@ -22,4 +24,43 @@ export interface DestinyEntityDefinition {
   index: number;
   redacted: boolean;
   blacklisted: boolean;
+}
+
+// ex: DestinyPlayerByBungieName
+export interface DestinyPlayerProfile {
+  supplementalDisplayName?: string;
+  iconPath: string;
+  crossSaveOverride: MembershipTypes;
+  applicableMembershipTypes: MembershipTypes[];
+  isPublic: boolean;
+  membershipType: MembershipTypes;
+  membershipId: string;
+  displayName: string;
+  bungieGlobalDisplayName: string;
+  bungieGlobalDisplayNameCode: number;
+}
+
+export interface DestinyProfilesList {
+  dateLastPlayed: string;
+  isOverridden: boolean;
+  isCrossSavePrimary: boolean;
+  crossSaveOverride: MembershipTypes;
+  applicableMembershipTypes: MembershipTypes[];
+  isPublic: boolean;
+  membershipType: MembershipTypes;
+  membershipId: string;
+  displayName: string;
+  bungieGlobalDisplayName: string;
+  bungieGlobalDisplayNameCode: number;
+}
+
+export interface ProfileWithErrors {
+  errorCode: number;
+  infoCard: DestinyPlayerProfile;
+}
+
+export interface DestinyLinkedProfile {
+  profiles: DestinyProfilesList[];
+  bnetMembership: DestinyPlayerProfile;
+  profilesWithErrors: ProfileWithErrors[];
 }
