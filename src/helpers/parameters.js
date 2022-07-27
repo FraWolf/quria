095d42536e5e473f5f574dc835e8ea281ab763f1
Francesco <frawolf@outlook.it>
2022-07-27 09:21:40 +0200

    fix: removed old js stuff

--- src/helpers/parameters.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  DestinyManifestDefinition: require("./Definitions/DestinyManifestDefinition"),
-  DestinyComponentType: require("./Definitions/DestinyComponentType"),
-  DestinyActivityModeType: require("./Definitions/DestinyActivityModeType"),
-  DestinyClass: require("./Definitions/DestinyClass"),
-};
