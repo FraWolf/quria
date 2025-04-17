d811d89fb766d5698b64083e0ac657a479e5bae4
FraWolf <frawolf@outlook.it>
2025-04-17 22:27:06 +0200

    feat: renamed fetch from http-request

--- src/adapters/http-request.ts
+++ src/adapters/http-request.ts
@@ -1,4 +1,4 @@
-import fetch from "isomorphic-unfetch";
+import unfetch from "isomorphic-unfetch";
 
 export async function httpRequest(
   url: string,
@@ -10,7 +10,7 @@ export async function httpRequest(
   let options: RequestInit = { method, headers };
   if (method === "POST") options.body = body;
 
-  let bungieRequest = await fetch(url, options);
+  let bungieRequest = await unfetch(url, options);
 
   return json ? await bungieRequest.json() : await bungieRequest.text();
 }
